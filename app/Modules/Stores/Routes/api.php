cafbf9b6fbbf9798f7d5759206f993cb0966855e
Rasujon3 <rasujon3@gmail.com>
2025-02-12 17:17:49 +0600

    store api done

---
+++ app/Modules/Stores/Routes/api.php
@@ -0,0 +1,18 @@
+<?php
+
+use App\Modules\Stores\Controllers\StoreController;
+use Illuminate\Support\Facades\Route;
+
+use App\Modules\City\Controllers\CityController;
+
+
+//Route::prefix('admin/cities')->middleware('auth:sanctum')->group(function () {
+Route::prefix('admin/stores')->group(function () {
+    Route::get('/', [StoreController::class, 'index']);          // List states
+    Route::get('/summary', [StoreController::class, 'getSummary']); // Get states summary
+    Route::get('/datatable', [StoreController::class, 'getStoresDataTable']);  // Get DataTable data
+    Route::get('/{store}', [StoreController::class, 'show']);    // View states
+    Route::post('/', [StoreController::class, 'store']);           // Create states
+    Route::put('/{store}', [StoreController::class, 'update']);  // Update states
+    Route::delete('/{store}', [StoreController::class, 'destroy']); // Delete states
+});
