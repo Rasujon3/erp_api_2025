f7e056b71186aa826eb4025e8a0a1c446346b2a9
Rasujon3 <rasujon3@gmail.com>
2025-02-17 15:34:22 +0600

    projects2 api done

--- app/Modules/Projects/Routes/api.php
+++ app/Modules/Projects/Routes/api.php
@@ -1,8 +1,16 @@
 <?php
 
+use App\Modules\Projects\Controllers\ProjectController;
 use Illuminate\Support\Facades\Route;
 
 
-Route::prefix('api/projects')->group(function () {
-
+//Route::prefix('admin/cities')->middleware('auth:sanctum')->group(function () {
+Route::prefix('admin/projects')->group(function () {
+    Route::get('/', [ProjectController::class, 'index']);          // List states
+    Route::get('/summary', [ProjectController::class, 'getSummary']); // Get states summary
+    Route::get('/datatable', [ProjectController::class, 'getSizesDataTable']);  // Get DataTable data
+    Route::get('/{project}', [ProjectController::class, 'show']);    // View states
+    Route::post('/', [ProjectController::class, 'store']);           // Create states
+    Route::put('/{project}', [ProjectController::class, 'update']);  // Update states
+    Route::delete('/{project}', [ProjectController::class, 'destroy']); // Delete states
 });
