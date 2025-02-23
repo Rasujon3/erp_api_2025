02641f11f86f70337b6b5a72c81171342db7edff
Rasujon3 <rasujon3@gmail.com>
2025-02-23 21:50:23 +0600

    home page issue solve

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,10 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 
 Route::get('/', function () {
-    return view('dashboard');
+    return "Welcome to Rapid Smart ERP";
 });
 
 
