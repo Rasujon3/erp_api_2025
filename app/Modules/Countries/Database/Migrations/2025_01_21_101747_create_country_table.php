aebed8bd0e397b8b2935dff7c3ca04924a85beac
Rasujon3 <rasujon3@gmail.com>
2025-02-26 19:53:42 +0600

    state api update done2

--- app/Modules/Countries/Database/Migrations/2025_01_21_101747_create_country_table.php
+++ app/Modules/Countries/Database/Migrations/2025_01_21_101747_create_country_table.php
@@ -12,8 +12,8 @@ return new class extends Migration
             $table->id();
             $table->string('code', 45);
             $table->string('name', 191);
-            $table->string('name_in_bangla', 191);
-            $table->string('name_in_arabic', 191);
+            $table->string('name_in_bangla', 191)->nullable();
+            $table->string('name_in_arabic', 191)->nullable();
             $table->boolean('is_default')->default(false);
             $table->boolean('draft')->default(false);
             $table->timestamp('drafted_at')->nullable();
